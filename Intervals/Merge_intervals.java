3c3cf618bbf804abaad8ad7e3e249dc37aacbfdc
Nithin Emmadishetti <nithin200133@gmail.com>
2025-07-16 21:57:18 +0530

    added

---
+++ Intervals/Merge_intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ArrayList<int[]> mergeOverlap(int[][] arr) {
+        // Code here
+        int n=arr.length;
+        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+        ArrayList<int[]> res=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int start=arr[i][0];
+            int end=arr[i][1];
+             if(res.size()==0||start>res.get(res.size()-1)[1])
+                {
+                    res.add(new int[]{start,end});
+                }
+                else{
+                    res.get(res.size()-1)[1]=Math.max(res.get(res.size()-1)[1],end);
+                     
+                }
+           
+        }
+        return res;
+    }
+}
