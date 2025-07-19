14bdf12329dd769f4a2889c0b1bba2bb22267fe1
Nithin Emmadishetti <nithin200133@gmail.com>
2025-07-19 17:57:12 +0530

    added

--- Graphs/Topological_Sort.java
+++ Graphs/Topological_Sort.java
@@ -1,3 +1,4 @@
+//Using Dfs
 class Solution {
     static void Dfs( Map<Integer,List<Integer>> adj,Stack<Integer> st,boolean vis[],int u)
     {
@@ -36,6 +37,54 @@ class Solution {
         {
             res.add(st.pop());
         }
+        return res;
+        
+    }
+}
+//Using BFS
+class Solution {
+    static void Bfs( Map<Integer,List<Integer>> adj,int[]indegree,ArrayList<Integer> res)
+    {
+        Queue<Integer> pq=new LinkedList<>();
+        for(int i=0;i<indegree.length;i++)
+        {
+            if(indegree[i]==0)
+            {
+                pq.offer(i);
+                
+            }
+        }
+        while(!pq.isEmpty())
+        {
+            int curr=pq.poll();
+            res.add(curr);
+        for(int ngbr:adj.getOrDefault(curr,new ArrayList<>()))
+        {
+            indegree[ngbr]--;
+            if(indegree[ngbr]==0)
+            {
+                pq.offer(ngbr);
+                 
+            }
+        
+        }
+        }
+    }
+    public static ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        ArrayList<Integer> res=new ArrayList<>();
+        int indegree[]=new int[V];
+        Map<Integer,List<Integer>> adj=new HashMap<>();
+        for(int edge[]:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            indegree[v]++;
+            adj.computeIfAbsent(u,k->new ArrayList<>()).add(v);
+        }
+                Bfs(adj,indegree,res);
+                
+       
         return res;
         
     }
