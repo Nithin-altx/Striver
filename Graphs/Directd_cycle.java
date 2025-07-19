cf2bee745aecce3a8709d002bb40314ec3e40d3f
Nithin Emmadishetti <nithin200133@gmail.com>
2025-07-19 18:25:01 +0530

    added

--- Graphs/Directd_cycle.java
+++ Graphs/Directd_cycle.java
@@ -46,4 +46,51 @@ class Solution {
     }
 }
 //BFS
-
+class Solution {
+    boolean Bfs( Map<Integer,List<Integer>> adj,int indeg[])
+    {
+        Queue<Integer> pq=new LinkedList<>();
+       for(int i=0;i<indeg.length;i++)
+       {
+           if(indeg[i]==0)
+           {
+               pq.offer(i);
+           }
+       }
+       int count=0;
+       while(!pq.isEmpty())
+       {
+           int curr=pq.poll();
+           count++;
+         for(int ngbr:adj.getOrDefault(curr,new ArrayList<>()))
+        {
+            indeg[ngbr]--;
+            if(indeg[ngbr]==0)
+            {
+                pq.offer(ngbr);
+            }
+           
+        }
+       }
+       return count!=indeg.length;
+        
+        
+      
+    }
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        Map<Integer,List<Integer>> adj=new HashMap<>();
+         int indeg[]=new int[V];
+        for(int edge[]:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            adj.computeIfAbsent(u,k->new ArrayList<>()).add(v);
+            indeg[v]++;
+        }
+        return Bfs(adj,indeg);
+   
+        
+        
+    }
+}
